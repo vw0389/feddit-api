dcfba926d345dae2a8e1a43af0c8a2fe72cc8daa
Victor <dev@vweinert.com>
2022-03-31 12:47:59 -0500

    initial commit

---
+++ src/main/java/com/feddit/fedditapi/FedditApiApplication.java
@@ -0,0 +1,13 @@
+package com.feddit.fedditapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FedditApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FedditApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/feddit/fedditapi/FedditApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feddit.fedditapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FedditApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
